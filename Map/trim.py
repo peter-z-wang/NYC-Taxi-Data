6e0704fc0ca2c3fd41ce4f5a06dc2fe5d72c228f
PeterWang <pwang0660@gmail.com>
2018-04-29 20:53:47 -0600

    Add marker dropping

--- Map/trim.py
+++ Map/trim.py
@@ -1,20 +1,19 @@
 #Python CSV Trimming script
+
 import csv
+import json
 
 file = open('locations.csv', 'w')
-
 
 with open("yellow_tripdata_2016-02.csv") as csvfile:
 	reader = csv.DictReader(csvfile)
 
 	for row in reader:
-		file.write(row['pickup_longitude'] + ',' + row['pickup_latitude'] + ',' + row['dropoff_longitude'] + ',' + row['dropoff_latitude'] + '\n')
+		file.write('['+ '"A"' + ',' + row['pickup_latitude'] + ',' + row['pickup_longitude'] + ']' + ',' + '\n')
 
 
 
 file.close()
-
-
 
 
 
